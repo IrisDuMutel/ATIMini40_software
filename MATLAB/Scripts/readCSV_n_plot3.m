3799f3164f8d739a4a05cf0e34e772f6f3e9d64b
Iris Du Mutel <58264427+IrisDuMutel@users.noreply.github.com>
2023-10-04 10:38:48 -0600

    Updates

--- MATLAB/Scripts/readCSV_n_plot3.m
+++ MATLAB/Scripts/readCSV_n_plot3.m
@@ -4,23 +4,30 @@ close all
 %%%% For extended files containing voltages and time vector
 folder = fileparts(pwd);
 file1 = '../LogFiles/20230916/log_20230916_2.csv';
-file1 = '../LogFiles/20231003/FT/find_PWM10_2.csv';
+file2 = '../LogFiles/20231002/FT/log_20231002_10inch1R_test3.csv';
 
 test1 = readtable(file1);
+test2 = readtable(file2);
 
 
 Weight_series = -[0.01,0.02,0.05,0.1,0.2,0.5,1]'*9.798;
 time1 = [0:1/test1{1,1}:1/test1{1,1}*(length(test1{:,2})-2)];
+time2 = [0:1/test2{1,1}:1/test2{1,1}*(length(test2{:,2})-2)];
 index = 0;
+offset = 20;
 
-Fx1 = (test1{2:end,index+1});% - test1{10,index+1});
-Fy1 = (test1{2:end,index+2});% - test1{10,index+2});
-Fz1 = (test1{2:end,index+3});% - test1{10,index+3});
-Mx1 = (test1{2:end,index+4});% - test1{10,index+4});
-My1 = (test1{2:end,index+5});% - test1{10,index+5});
-Mz1 = (test1{2:end,index+6});% - test1{10,index+6});
-
-
+Fx1 = (test1{2:end,index+1});% - test1{offset,index+1});
+Fy1 = (test1{2:end,index+2});% - test1{offset,index+2});
+Fz1 = (test1{2:end,index+3});% - test1{offset,index+3});
+Mx1 = (test1{2:end,index+4});% - test1{offset,index+4});
+My1 = (test1{2:end,index+5});% - test1{offset,index+5});
+Mz1 = (test1{2:end,index+6});% - test1{offset,index+6});
+Fx2 = (test2{2:end,index+1});% - test2{offset,index+1});
+Fy2 = (test2{2:end,index+2});% - test2{offset,index+2});
+Fz2 = (test2{2:end,index+3});% - test2{offset,index+3});
+Mx2 = (test2{2:end,index+4});% - test2{offset,index+4});
+My2 = (test2{2:end,index+5});% - test2{offset,index+5});
+Mz2 = (test2{2:end,index+6});% - test2{offset,index+6});
 
 % figure()
 % hold on;grid on;
@@ -98,58 +105,120 @@ wpass_fz = 0.2;%[0.1];
 wpass_mx = 0.5;%[2.5, 3.5];
 wpass_my = 0.5;%[56, 60];
 wpass_mz = 0.5;%[0.1];
-filtered_Fx1 = lowpass(Fx1,wpass_fx,150);
-filtered_Fy1 = lowpass(Fy1,wpass_fy,150);
-filtered_Fz1 = lowpass(Fz1,wpass_fz,150);
-filtered_Mx1 = lowpass(Mx1,wpass_mx,150);
-filtered_My1 = lowpass(My1,wpass_my,150);
-filtered_Mz1 = lowpass(Mz1,wpass_mz,150);
+filtered_Fx1 = lowpass(Fx1,wpass_fx,sampl_f);
+filtered_Fy1 = lowpass(Fy1,wpass_fy,sampl_f);
+filtered_Fz1 = lowpass(Fz1,wpass_fz,sampl_f);
+filtered_Mx1 = lowpass(Mx1,wpass_mx,sampl_f);
+filtered_My1 = lowpass(My1,wpass_my,sampl_f);
+filtered_Mz1 = lowpass(Mz1,wpass_mz,sampl_f);
+
+filtered_Fx2 = lowpass(Fx2,wpass_fx,sampl_f);
+filtered_Fy2 = lowpass(Fy2,wpass_fy,sampl_f);
+filtered_Fz2 = lowpass(Fz2,wpass_fz,sampl_f);
+filtered_Mx2 = lowpass(Mx2,wpass_mx,sampl_f);
+filtered_My2 = lowpass(My2,wpass_my,sampl_f);
+filtered_Mz2 = lowpass(Mz2,wpass_mz,sampl_f);
+
+% figure()
+% subplot(3,1,1)
+% hold on
+% plot(time1,Fx1,'linewidth',2)
+% plot(time1,filtered_Fx1,'linewidth',2)
+% legend('Fx','Fx filtered')
+% grid on
+% ylabel('Fx [N]')
+% subplot(3,1,2)
+% hold on
+% plot(time1,Fy1,'linewidth',2)
+% plot(time1,filtered_Fy1,'linewidth',2)
+% legend('Fy','Fy filtered')
+% grid on
+% ylabel('Fy [N]')
+% subplot(3,1,3)
+% hold on
+% plot(time1,Fz1,'linewidth',2)
+% plot(time1,filtered_Fz1,'linewidth',2)
+% legend('Fz','Fz filtered')
+% grid on
+% ylabel('Fz [N]')
+% xlabel('Time [s]')
+% grid on
 
 figure()
 subplot(3,1,1)
 hold on
-plot(time1,Fx1,'linewidth',2)
-plot(time1,filtered_Fx1,'linewidth',2)
-legend('Fx','Fx filtered')
+plot(time1,filtered_Fx1,'linewidth',1.5)
+plot(time2,filtered_Fx2,'linewidth',1.5)
+legend('Fx_{noWall}','Fx 1R')
+ylim([-3 3])
 grid on
 ylabel('Fx [N]')
 subplot(3,1,2)
 hold on
-plot(time1,Fy1,'linewidth',2)
-plot(time1,filtered_Fy1,'linewidth',2)
-legend('Fy','Fy filtered')
+plot(time1,filtered_Fy1,'linewidth',1.5)
+plot(time2,filtered_Fy2,'linewidth',1.5)
+legend('Fy_{noWall}','Fy 1R')
+ylim([-3 3])
 grid on
 ylabel('Fy [N]')
 subplot(3,1,3)
 hold on
-plot(time1,Fz1,'linewidth',2)
-plot(time1,filtered_Fz1,'linewidth',2)
-legend('Fz','Fz filtered')
+plot(time1,filtered_Fz1,'linewidth',1.5)
+plot(time2,filtered_Fz2,'linewidth',1.5)
+legend('Fz_{noWall}','Fz 1R')
 grid on
 ylabel('Fz [N]')
 xlabel('Time [s]')
 grid on
 
+% figure()
+% subplot(3,1,1)
+% hold on
+% plot(time1,Mx1,'linewidth',2)
+% plot(time1,filtered_Mx1,'linewidth',2)
+% legend('Mx','Mx filtered')
+% grid on
+% ylabel('Mx [N·m]')
+% subplot(3,1,2)
+% hold on
+% plot(time1,My1,'linewidth',2)
+% plot(time1,filtered_My1,'linewidth',2)
+% legend('My','My filtered')
+% grid on
+% ylabel('My [N·m]')
+% subplot(3,1,3)
+% hold on
+% plot(time1,Mz1,'linewidth',2)
+% plot(time1,filtered_Mz1,'linewidth',2)
+% legend('Mz','Mz filtered')
+% grid on
+% ylabel('Mz [N·m]')
+% xlabel('Time [s]')
+% grid on
+
 figure()
 subplot(3,1,1)
 hold on
-plot(time1,Mx1,'linewidth',2)
-plot(time1,filtered_Mx1,'linewidth',2)
-legend('Mx','Mx filtered')
+plot(time1,filtered_Mx1,'linewidth',1.5)
+plot(time2,filtered_Mx2,'linewidth',1.5)
+legend('Mx_{noWall}','Mx 1R')
+ylim([-0.1 0.15])
 grid on
 ylabel('Mx [N·m]')
 subplot(3,1,2)
 hold on
-plot(time1,My1,'linewidth',2)
-plot(time1,filtered_My1,'linewidth',2)
-legend('My','My filtered')
+plot(time1,filtered_My1,'linewidth',1.5)
+plot(time2,filtered_My2,'linewidth',1.5)
+legend('My_{noWall}','My 1R')
+ylim([-0.1 0.15])
 grid on
 ylabel('My [N·m]')
 subplot(3,1,3)
 hold on
-plot(time1,Mz1,'linewidth',2)
-plot(time1,filtered_Mz1,'linewidth',2)
-legend('Mz','Mz filtered')
+plot(time1,filtered_Mz1,'linewidth',1.5)
+plot(time2,filtered_Mz2,'linewidth',1.5)
+legend('Mz_{noWall}','Mz 1R')
+ylim([-0.1 0.15])
 grid on
 ylabel('Mz [N·m]')
 xlabel('Time [s]')
